940a91cc25043b72c442202ed310bc02e6cb01eb
Olivia Cho <oliviac.nextacademy@gmail.com>
2021-03-20 11:11:18 -0400

    Initial Commit

---
+++ GitHubTest IVY/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubTest IVY
+//
+//  Created by Mac 1 on 3/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
